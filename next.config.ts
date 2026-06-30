9c58c6ff25cf0918a58df2987a702b9a909f46f3
Naveed Dumasia <naveed.dumasia@gmail.com>
2026-07-01 01:53:34 +0530

    Disable next/image optimization to avoid sharp dependency on Hostinger Node hosting

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "i.pravatar.cc" },
     ],
+    unoptimized: true,
   },
 };
 
